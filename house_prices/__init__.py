3a70020e77194dc17a36b56cb0f2a668503227ac
navaraja20 <navaraja2001@gmail.com>
2025-04-11 14:24:09 +0200

    created the house_prices package

--- house_prices/__init__.py
+++ house_prices/__init__.py
@@ -2,5 +2,5 @@
 from .preprocess import get_feature_names
 from .train import build_model
 from .inference import make_predictions
-
+__version__ = "0.1.0"
 __all__ = ['get_feature_names', 'build_model', 'make_predictions']
---
+++ test.py
@@ -0,0 +1,3 @@
+from house_prices import build_model, make_predictions
+print(build_model.__module__)
+print(make_predictions.__module__)
